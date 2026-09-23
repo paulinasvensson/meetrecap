3a77a5979ee08935b22fa35fda7cfe8d574a4df8
paulinasvensson <paulinasvensson01@gmail.com>
2026-09-23 22:46:24 +0200

    Deploy meetrecap

--- entitlements.py
+++ entitlements.py
@@ -7,7 +7,7 @@ import httpx
 from fastapi import Header, HTTPException
 
 NDS_API_BASE = 'http://localhost:8000'
-VENTURE_ID = '24fa8ea07a6342b9adaf6ce95dac543d'
+VENTURE_ID = '76b326fe87224e788c4fbdf690f2249e'
 
 
 async def require_pro(x_license_key: str = Header(default="")) -> str:
